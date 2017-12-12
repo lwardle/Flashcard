4acfb2480f7fd0ad69c4817f6cdd5d43310cb670
lwardle <32503922+lwardle@users.noreply.github.com>
2017-12-11 21:56:09 -0800

    Update Flashcard.py
    Fleshed out the save and load functions in tkinter rather than shell. Added notes on functions needed to handle flashcards. Reorganized GUI to look neater.

--- Flashcard.py
+++ Flashcard.py
@@ -34,22 +34,71 @@ class Box(object):
         pass
     
 #TODO: save/load functions
-def save():
-    input("Are you sure? hit ctrl-c to cancel, or press enter to continue.")
+def savePrompt():
+    save_window = Toplevel()
+    warning = Label(save_window, text = "Are you sure you want to save?\nThis will overwrite existing data.")
+    affirm_button = Button(save_window, text = "Yes", command = lambda: saveFunction(save_window))
+    cancel_button = Button(save_window, text = "No", command = lambda: save_window.destroy())
+
+    warning.grid(row = 0, column = 0, columnspan = 2, padx = 20, pady = 10)
+    affirm_button.grid(row = 1, column = 0)
+    cancel_button.grid(row = 1, column = 1)
+
+def saveFunction(save_window):
     pickle_out = open("flashcard_data.pickle", "wb")
     pickle.dump(all_cards, pickle_out)
     pickle_out.close()
+    save_window.destroy()
 
-def load():
+def loadPrompt():
+    load_window = Toplevel()
+    warning = Label(load_window, text = "Are you sure you want to save?\nThis will overwrite existing data.")
+    affirm_button = Button(load_window, text = "Yes", command = lambda: loadFunction(load_window))
+    cancel_button = Button(load_window, text = "No", command = lambda: load_window.destroy())
+
+    warning.grid(row = 0, column = 0, columnspan = 2, padx = 20, pady = 10)
+    affirm_button.grid(row = 1, column = 0)
+    cancel_button.grid(row = 1, column = 1)
+
+def loadFunction(load_window):
     global all_cards
-    input("Are you sure? hit ctrl-c to cancel, or press enter to continue.")
     try:
         pickle_in = open("flashcard_data.pickle", "rb")
         all_cards = pickle.load(pickle_in)
     except:
-        print("I couldn't load your cards properly. Just a guess: is there a 'flashcard_data.pickle' file in the same folder as the flashcard app? There should be.")
+        load_error_window = Toplevel()
+        error_message = Label(load_error_window, wraplength = 200, text = "I couldn't load your cards properly. Just a guess: is there a 'flashcard_data.pickle' file in the same folder as the flashcard app? There should be.")
+        okay_button = Button(load_error_window, text = "Okay", command = lambda: load_error_window.destroy())
+
+        error_message.pack()
+        okay_button.pack()
+    load_window.destroy()
 
 #TODO: button functions
+
+"""
+
+generateStack():
+	pull all cards that have review_flag = True
+
+showAnswer():
+	change card_back to current card's response
+
+markCorrect():
+	set current card's review flag to false
+	change current card's location to location + 1
+	set card_back to an empty string
+	remove current card from study stack
+	change card_front to new card's prompt
+
+markIncorrect():
+	set current card's review flag to false
+	change current card's location to 0
+	set card_back to an empty string
+	remove current card from study stack
+	change card_front to new card's prompt
+
+"""
 
 def finalizeCard(card_prompt, card_response, creation_window):
     new_card = Flash_Card(card_prompt.get(), card_response.get())
@@ -75,8 +124,8 @@ def creationPopup():
 def deletionPopup():
     deletion_window = Toplevel()
     warning = Label(deletion_window, text = "Are you sure?")
-    affirm_button = Button(deletion_window, text = "Yes", callback = None)
-    cancel_button = Button(deletion_window, text = "No", callback = None)
+    affirm_button = Button(deletion_window, text = "Yes", command = None)
+    cancel_button = Button(deletion_window, text = "No", command = None)
 
     warning.grid(row = 0, column = 0, columnspan = 2, padx = 20, pady = 10)
     affirm_button.grid(row = 1, column = 0)
@@ -94,7 +143,6 @@ index_card_image = PhotoImage(file = os.path.abspath(".") + "\index_card.gif")
 #TODO: create GUI elements
 card_front = Label(root, text = "In what year did Colombus set sail for the Americas?", wraplength = 350, font = ("Arial", 24, "bold"), image = index_card_image, compound = CENTER)
 card_back = Label(root, text = "1492", wraplength = 350, font = ("Arial", 24, "bold"), image = index_card_image, compound = CENTER)
-generate_stack = Button(root, text = "Generate Stack")
 mark_right = Button(root, text = "Mark Correct")
 mark_wrong = Button(root, text = "Mark Incorrect")
 show_answer = Button(root, text = "Show Answer")
@@ -102,17 +150,20 @@ create_new_card = Button(root, text = "Create New Card", command = creationPopup
 cards_left = Label(root, text = "Number of cards left:")
 total_cards = Label(root, text = "Total number of cards:")
 delete_card = Button(root, text = "Delete This Card", command = deletionPopup)
+save_button = Button(root, text = "Save", command = savePrompt)
+load_button = Button(root, text = "Load", command = loadPrompt)
 
 #TODO: arrange GUI elements
-card_front.grid(row = 0, column = 0, rowspan = 2, columnspan = 2, padx = 10, pady = 10)
-card_back.grid(row = 0, column = 2, rowspan = 2, columnspan = 2, padx = 10, pady = 10)
-delete_card.grid(row = 0, column = 5)
-create_new_card.grid(row = 0, column = 4)
-generate_stack.grid(row = 2, column = 0)
-cards_left.grid(row = 1, column = 4)
-show_answer.grid(row = 2, column = 1)
-mark_right.grid(row = 2, column = 2)
-mark_wrong.grid(row = 2, column = 3)
-total_cards.grid(row = 2, column = 4)
+card_front.grid(row = 0, column = 0, rowspan = 6)
+card_back.grid(row = 0, column = 1, rowspan = 6)
+delete_card.grid(row = 4, column = 3)
+create_new_card.grid(row = 4, column = 2)
+cards_left.grid(row = 2, column = 2, columnspan = 2)
+show_answer.grid(row = 0, column = 2, columnspan = 2)
+mark_right.grid(row = 1, column = 2)
+mark_wrong.grid(row = 1, column = 3)
+total_cards.grid(row = 3, column = 2, columnspan = 2)
+save_button.grid(row = 5, column = 2)
+load_button.grid(row = 5, column = 3)
 
 root.mainloop()
